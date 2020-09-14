d6a54d47f48a1fd97a4152e2ab1a57dd75d83d02
rrandriamana <rrandriamana@gmail.com>
2020-09-14 19:18:49 +0200

    dependencies changed

--- software/setup.py
+++ software/setup.py
@@ -14,7 +14,7 @@ setup(name='poppy-ergo-jr',
       version=version(),
       packages=find_packages(),
 
-      install_requires=['pypot >= 4.0.0', 'hampy'],
+      install_requires=['pypot @ git+https://github.com/rrandriamana/pypot@scratch#egg=pypot', 'hampy'],
 
       include_package_data=True,
       exclude_package_data={'': ['README', '.gitignore']},
